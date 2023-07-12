a4216089140c00d8d5686fb482ea3ec89a7e491d
mirrdy <128653609+Yurisoo206@users.noreply.github.com>
2023-07-12 17:59:24 +0900

    주문서 세팅

--- Assets/3.Script/UI/OrderSheetMove.cs
+++ Assets/3.Script/UI/OrderSheetMove.cs
@@ -6,13 +6,24 @@ public class OrderSheetMove : MonoBehaviour
 {
     public Orderlist orderlist;
 
+    private GameObject test;
+
     void Start()
     {
         orderlist = FindObjectOfType<Orderlist>();
+        test = GetComponent<GameObject>();
+
     }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, orderlist.transform.position, 0.2f);
+        //transform.position = Vector3.Lerp(transform.position, orderlist.sheetPos.transform.position, 0.2f);
+    }
+
+
+    public void SavePos()
+    {
+        test = orderlist.sheetPos;
+
     }
 }
--- Assets/3.Script/UI/Orderlist.cs
+++ Assets/3.Script/UI/Orderlist.cs
@@ -20,7 +20,6 @@ public class Orderlist : MonoBehaviour
 
     public GameObject order;
 
-
     public GameObject sheet1_prefed;
     public GameObject sheet2_prefed;
 
@@ -32,19 +31,24 @@ public class Orderlist : MonoBehaviour
     private int num = 0;
 
     public bool check = false;
+    public bool test = false;
+
+
+    //일단 sheet 생성시 받아갈 위치 
+    public GameObject sheetPos;
+
 
     private void Start()
     {
         dish = FindObjectOfType<CompleteDish>();
-        setList = new GameObject[3];
-
-        setList[0] = order1;
+        setList = new GameObject[5];
+        
     }
-
 
     void Update()
     {
-        StartOrder();
+        
+        //StartOrder();
         if (Input.GetKeyDown(KeyCode.Q))
         {
             sheet1 = sheet1_prefed;
@@ -55,17 +59,20 @@ public class Orderlist : MonoBehaviour
         {
             OrderClear();
         }
-
     }
 
     public void StartOrder()
     {
-        sheet1.transform.position = Vector3.Lerp(sheet1.transform.position, order1.transform.position, 0.2f);
-        sheet2.transform.position = Vector3.Lerp(sheet2.transform.position, order2.transform.position, 0.2f);
+        //sheet1.transform.position = Vector3.Lerp(sheet1.transform.position, order1.transform.position, 0.2f);
+        //sheet2.transform.position = Vector3.Lerp(sheet2.transform.position, order2.transform.position, 0.2f);
         if (num < 1)
         {
             orderList.Add(sheet1);
             orderList.Add(sheet2);
+
+            setList[0] = sheet1;
+            setList[1] = sheet2;
+
             num++;
         }
     }
@@ -87,31 +94,125 @@ public class Orderlist : MonoBehaviour
     public void RandomOrder()
     {
         RandomSheet();
-        orderList.Add(randomSheet);
-        order = Instantiate(randomSheet, respawn.position, respawn.rotation);
-        order.transform.SetParent(gameObject.transform);
-        for (int i = 0; i < orderList.Count; i++)
-        {        
-            Debug.Log("배열 확인 : " + orderList[i].name);
+
+        if (orderList.Count < 5)
+        {
+            order = Instantiate(randomSheet, respawn.position, respawn.rotation);
+            orderList.Add(order);
+            order.transform.SetParent(gameObject.transform);
+
+            for (int i = 0; i < setList.Length; i++)
+            {
+                if (setList[i] == null)
+                {
+                    setList[i] = order.gameObject;
+                    break;
+                }
+
+                Debug.Log("모르겠다고 : " + setList[i].gameObject);
+            }
+
+            for (int i = 0; i < orderList.Count; i++)
+            {
+                if (order == orderList[0])
+                {
+                    sheetPos = order1;
+                    order = setList[0];
+                    order.transform.position = sheetPos.transform.position;
+                }
+                else if (order == orderList[1])
+                {
+                    sheetPos = order2;
+                    order = setList[1];
+                    order.transform.position = sheetPos.transform.position;
+                }
+                else if (order == orderList[2])
+                {
+                    sheetPos = order3;
+                    order = setList[2];
+                    order.transform.position = sheetPos.transform.position;
+                }
+                else if (order == orderList[3])
+                {
+                    sheetPos = order4;
+                    order = setList[3];
+                    order.transform.position = sheetPos.transform.position;
+                }
+                else if (order == orderList[4])
+                {
+                    sheetPos = order5;
+                    order = setList[4];
+                    order.transform.position = sheetPos.transform.position;
+                }
+            }
+            check = true;
         }
     }
      
     public void OrderClear()
     {
-        if (check)
+        for (int i = 0; i < orderList.Count; i++)
         {
-            for (int i = 0; i < orderList.Count; i++)
+            if (orderList[i].tag == sheet1.tag)
             {
-                if (orderList[i] == sheet1)
-                {
-                    Debug.Log("이거 삭제해" + orderList[i].name);
-                    orderList.RemoveAt(i);
+                //Debug.Log("이거 삭제해" + orderList[i].name);
+                orderList.RemoveAt(i);
 
-                    break;
-                }
+                Destroy(setList[i].gameObject);
+                
+                Debug.Log("이거 삭제해" + setList[i] + i);
+                setList[i] = null;
+                check = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < orderList.Count; i++)
+        {
+            if (setList[i] = null)
+            {
+                Debug.Log("으어어");
+                //Debug.Log("아 지친다 진짜");
+                setList[i] = setList[i + 1];
+                setList[i + 1] = null;
             }
 
-            check = false;
+            Debug.Log("오더" + orderList[i] +i);
         }
-    }                                                                           
+
+        //if (check)
+        //{
+        //    for (int i = 0; i < orderList.Count; i++)
+        //    {
+        //        if (setList[i] == orderList[0])
+        //        {
+        //            sheetPos = order1;
+        //            setList[i].transform.position = sheetPos.transform.position;
+        //        }
+        //        else if (setList[i] == orderList[1])
+        //        {
+        //            sheetPos = order2;
+        //            setList[i].transform.position = sheetPos.transform.position;
+        //        }
+        //        else if (setList[i] == orderList[2])
+        //        {
+        //            sheetPos = order3;
+        //            setList[i].transform.position = sheetPos.transform.position;
+        //        }
+        //        else if (setList[i] == orderList[3])
+        //        {
+        //            sheetPos = order4;
+        //            setList[i].transform.position = sheetPos.transform.position;
+        //        }
+        //        else if (setList[i] == orderList[4])
+        //        {
+        //            sheetPos = order5;
+        //            setList[i].transform.position = sheetPos.transform.position;
+        //        }
+        //    }
+
+        //    check = false;
+        //}
+
+    }
 }
